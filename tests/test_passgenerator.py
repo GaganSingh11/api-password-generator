f96a94edbf42f0191c66d1803befc3aee382e974
GaganSingh11 <gsmultani11@gmail.com>
2022-11-28 10:12:49 +0100

    added 2 more testcase

--- tests/test_passgenerator.py
+++ tests/test_passgenerator.py
@@ -1,5 +1,53 @@
+import pytest
 from app.password_generator import PassGenerator
 
-def test_generate_pass():
+def test_psw_length():
     passwd = PassGenerator(length=10, upper=True, lower=True, number=True, specialchar=True).generate_psw()
-    assert len(passwd) == 10    
+    assert len(passwd) == 10
+
+def test_uppercase():
+
+    uppcase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                "T", "U", "V", "W", "X", "Y", "Z"]
+    with_upper = False
+    without_upper = False
+
+    psw_with_upper = PassGenerator(length=10, upper=True, lower=True, number=True, specialchar=True).generate_psw()
+    psw_without_upper = PassGenerator(length=10, upper=False, lower=True, number=True, specialchar=True).generate_psw()
+
+    for letter in uppcase:
+        if letter in psw_with_upper:
+            with_upper = True
+            break
+    
+    for letter in uppcase:
+        if letter in psw_without_upper:
+            without_upper = True
+            break 
+
+    assert with_upper == True
+    assert without_upper == False
+
+def test_lowercase():
+
+    lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                "t", "u", "v", "w", "x", "y", "z"]
+
+    with_lower = False
+    without_lower = False
+
+    psw_with_lower = PassGenerator(length=10, upper=True, lower=True, number=True, specialchar=True).generate_psw()
+    psw_without_lower = PassGenerator(length=10, upper=True, lower=False, number=True, specialchar=True).generate_psw()
+
+    for letter in lowercase:
+        if letter in psw_with_lower:
+            with_lower = True
+            break
+    
+    for letter in lowercase:
+        if letter in psw_without_lower:
+            without_lower = True
+            break 
+
+    assert with_lower == True
+    assert without_lower == False
