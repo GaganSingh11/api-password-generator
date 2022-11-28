d9db01bb020fec024e3164463849443ae7295f69
GaganSingh11 <gsmultani11@gmail.com>
2022-11-28 11:37:56 +0100

    tests for endpoint

---
+++ tests/test_endpoint.py
@@ -0,0 +1,30 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 404
+
+def test_non_existing_path():
+    response = client.get("/non/exisiting/path")
+    assert response.status_code == 404
+
+def test_no_flag():
+    response = client.get("/api/v1/password")
+    assert response.status_code == 406
+    assert response.json() == {"detail": "Switch on atleast one flag from uppercase, lowercase, number or special_char"}
+
+def test_psw_length_limit():
+    response = client.get("/api/v1/password?pwd_length=201&number=true")
+    assert response.status_code == 406
+    assert response.json() == {"detail": "Password length exceeded limit of 200"}
+
+def test_psw_default_length():
+    response = client.get("/api/v1/password?number=true")
+    psw = response.json()
+    assert len(psw["password"]) == 10
+
+# def test_flag_effectivness():
+#     pass
